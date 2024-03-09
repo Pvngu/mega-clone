06986f6c189d6026b65ae9b98d11a749202f7001
Pvngu <omargt151102@gmail.com>
2024-03-09 01:42:53 -0800

    add footer

---
+++ src/components/ToggleTheme.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ToggleTheme() {
+  return (
+    <button id='toggleTheme'>
+        <i class='bx bx-sun text-2xl'></i>
+    </button>
+  )
+}
+
+export default ToggleTheme
