0f04f8e4389e8adfe9bd464e289252ebecb5b3c7
Pvngu <omargt151102@gmail.com>
2024-03-09 17:08:10 -0800

    add toggleTheme button

--- src/components/ToggleTheme.jsx
+++ src/components/ToggleTheme.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 function ToggleTheme() {
+  const [theme, setTheme] = useState(localStorage.getItem("theme") ?? "light");
+
+  useEffect(() => {
+
+  });
+
+  const handleClick = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  }
+
+  useEffect(() => {
+    if(theme === "dark") document.documentElement.classList.add("dark")
+    else document.documentElement.classList.remove("dark");
+  localStorage.setItem("theme", theme);
+  },[theme])
+
   return (
-    <button id='toggleTheme'>
-        <i class='bx bx-sun text-2xl'></i>
+    <button onClick={handleClick}>
+        <i className={`bx bx-${theme === 'light' ? 'moon' : 'sun'} text-2xl`}></i>
     </button>
   )
 }
